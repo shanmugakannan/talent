7dd0fac5fab62ee96574eb889dd66c5c1450fcbd
Shanmugakannan <shanmugakannan@gmail.com>
2017-03-24 01:53:23 +0530

    Edit View - Initial

--- src/app/grid/data-table.component.ts
+++ src/app/grid/data-table.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit, Input,Output,EventEmitter } from '@angular/core';
+import { Component, OnInit, Input,Output,EventEmitter,trigger,state,style,transition,animate } from '@angular/core';
 import { ColumnComponent } from './column.component';
 import { GridComponent } from './grid.component';
 
 @Component({
   selector: 'datatable',
   templateUrl: 'data-table.component.html',
-  styleUrls: ['grid.component.css']
+  styleUrls: ['grid.component.css'],
+   animations: [
+    trigger('pageState', [
+      state('view', style({
+        width: "95%"
+      })),
+      state('edit',   style({
+          width: "50%"
+      })),
+      transition('view => edit', animate('500ms')),
+      transition('edit => view', animate('500ms'))
+    ])
+  ]
 })
 export class DatatableComponent  implements OnInit  {
  constructor(){    
  }
-
+  pageState:string;
   @Input() dataset; 
   @Output() ratebtnClick = new EventEmitter<string>();
-
+  isEditMode:boolean;
   columns: ColumnComponent[] = [];
  
   addColumn(column:ColumnComponent){
@@ -23,10 +35,19 @@ export class DatatableComponent  implements OnInit  {
   getData(){
       return this.dataset;
     }
-    
+   
  OnratebtnClick(args:string){
+  this.changState('edit');  
   this.ratebtnClick.emit(args);
  }   
-  ngOnInit() {
+
+ private changState(state:string){
+      this.pageState = state;
+      this.isEditMode = true;
   }
+
+  ngOnInit() {
+    this.pageState='view';
+  }
+
 }
--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,14 +1,4 @@
-import { Component, 
-         OnInit,
-         trigger,
-         state,
-         style,
-         transition,
-         animate,
-         AfterViewInit, 
-         ViewChild 
-        } 
-from '@angular/core';
+import { Component,OnInit,AfterViewInit,ViewChild } from '@angular/core';
 import { DatatableComponent } from './data-table.component';
 import { ApiService } from "../services/api.service";
 import { GetEmployeeRatingService } from "../services/get-employee-rating.service";
@@ -16,51 +6,29 @@ import { GetEmployeeRatingService } from "../services/get-employee-rating.servic
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
-  styleUrls: ['./grid.component.css'],
-   animations: [
-    trigger('pageState', [
-      state('view', style({
-        width: "95%"
-      })),
-      state('edit',   style({
-        width: "25%"
-      })),
-      transition('inactive => active', animate('500ms')),
-      transition('active => inactive', animate('500ms'))
-    ])
-  ]
+  styleUrls: ['./grid.component.css']
 })
 
 export class GridComponent implements AfterViewInit  {
   @ViewChild(DatatableComponent)
   private grid : DatatableComponent;
 
-  pageState='view';
   Data : any = [];
   constructor(private dataservice :GetEmployeeRatingService){
     this.dataservice.getEmployeeRating()
         .subscribe(res => this.Data = res.json())
   }
 
-private changState(state:string){
-      this.pageState = state;
-  }
-
 OnRateEmployee(id:string)
 {
-  this.changState('edit');
-  if(this.pageState == "edit"){
-    this.grid.columns.forEach(element => {
-     
-     if(element.value == "emp_no" || element.value == "emp_name"){
+   this.grid.columns.forEach(element => {
+     if(element.value == "emp_name" || element.value == "emp_no"){
         element.visible = true;
      }
      else{
        element.visible = false;
      }
     });
-      
-  }
 }
 
 ngAfterViewInit() {  
